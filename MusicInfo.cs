44825e0c01054f61b63ab9ace8bc07de6278af83
Harris Zhang <harriszhang@live.cn>
2017-11-22 17:05:57 +0800

    Create MusicInfo.cs

---
+++ MusicInfo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicClassifyLibrary
+{
+    public class MusicInfo
+    {
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Album { get; set; }
+
+        public string Year { get; set; }
+    }
+}
