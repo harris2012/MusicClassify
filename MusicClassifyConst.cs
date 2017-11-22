56a6a4dd3e161909ae1a87f960bbf6bacde46847
Harris Zhang <harriszhang@live.cn>
2017-11-22 17:06:24 +0800

    Create MusicClassifyConst.cs

---
+++ MusicClassifyConst.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicClassifyLibrary
+{
+    class MusicClassifyConst
+    {
+        public const int ContentMaxLength = 100;
+
+        public const string NotAMp3File = "Not a mp3 file";
+
+        public const string GB2312 = "GB2312";
+
+        /// <summary>
+        /// Artist
+        /// </summary>
+        public const string TPE1 = "TPE1";
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        public const string TIT2 = "TIT2";
+
+        /// <summary>
+        /// Alburn
+        /// </summary>
+        public const string TALB = "TALB";
+
+        /// <summary>
+        /// Year
+        /// </summary>
+        public const string TYER = "TYER";
+
+        public const string UnknownArtist = "Unknown Artist";
+
+        public const string UnknownTitle = "Unknown Title";
+
+        public const string UnknownAlburn = "Unknown Ablurn";
+    }
+}
